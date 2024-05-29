7872ea9bb4b67d69759c5386ee52bd5e8a47613c
ElifGokc <elifgokcekoksal@gmail.com>
2024-05-29 22:05:54 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Muhasebe p1= new Muhasebe("Ayşe Özcan",1000,30);
+        Muhasebe p2= new Muhasebe("Ali Çay",500,10);
+        Muhasebe p3= new Muhasebe("Ali Koç",400,28);
+        Muhasebe p4= new Muhasebe("Aylin Gün",2000,15);
+
+
+        double maas1=p1.maasHesapla();
+        double maas2=p2.maasHesapla();
+        double maas3=p3.maasHesapla();
+        double maas4=p4.maasHesapla();
+
+
+        System.out.println("Personel maaşları listesi:\n");
+        System.out.println(p1.calisanAdSoyad+" isimli personelin maas bilgisi= "+maas1+"\n");
+        System.out.println(p2.calisanAdSoyad+" isimli personelin maas bilgisi= "+maas2+"\n");
+        System.out.println(p3.calisanAdSoyad+" isimli personelin maas bilgisi= "+maas3+"\n");
+        System.out.println(p4.calisanAdSoyad+" isimli personelin maas bilgisi= "+maas4+"\n");
+
     }
 }
--- src/Muhasebe.java
+++ src/Muhasebe.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Muhasebe {
+public class Muhasebe {
+
+    public String calisanAdSoyad;
+    public double gunlukMaas;
+    public int calisilanGun;
+
+    Muhasebe(String calisanAdSoyad,double gunlukMaas,int calisilanGun){
+        this.calisanAdSoyad=calisanAdSoyad;
+        this.gunlukMaas=gunlukMaas;
+        this.calisilanGun=calisilanGun;
+
+    }
+    public double maasHesapla() {
+        double toplamMaas = gunlukMaas * calisilanGun;
+        int prim;
+
+        prim= (calisilanGun > 25) ? (calisilanGun - 25) * 1000 : 0;
+
+        toplamMaas+=prim;
+
+        return toplamMaas;
+
+    }
+
+
 }
